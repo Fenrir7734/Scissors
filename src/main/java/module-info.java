8c4085141b653b5abf9f7f1cc422cdc7944276b3
Fenrir7734 <hitex1999@gmail.com>
2021-08-23 13:50:10 +0200

    logger config

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module com.fenrir.scissors {
     requires javafx.fxml;
     requires javafx.graphics;
     requires javafx.base;
+    requires org.slf4j;
 
     opens com.fenrir.scissors.controllers to javafx.fxml;
     exports com.fenrir.scissors;
