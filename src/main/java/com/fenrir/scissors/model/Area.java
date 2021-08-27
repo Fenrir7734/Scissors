f7bbad0e97146caea53e848a291b7437d30c5526
Fenrir7734 <hitex1999@gmail.com>
2021-08-27 13:11:28 +0200

    clean up

---
+++ src/main/java/com/fenrir/scissors/model/Area.java
@@ -0,0 +1,2 @@
+package com.fenrir.scissors.model;public class Area {
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,10 +2,15 @@ module com.fenrir.scissors {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.graphics;
+    requires javafx.swing;
     requires javafx.base;
     requires org.slf4j;
     requires org.json;
+    requires java.desktop;
 
     opens com.fenrir.scissors.controllers to javafx.fxml;
     exports com.fenrir.scissors;
+    exports com.fenrir.scissors.model;
+    exports com.fenrir.scissors.test;
+    exports com.fenrir.scissors.controllers;
 }
