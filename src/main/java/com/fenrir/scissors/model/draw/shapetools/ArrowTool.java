6089e2508aa2932ac5949aa10191b6af61094c8b
Fenrir7734 <hitex1999@gmail.com>
2021-09-02 12:27:53 +0200

    arrow tool

--- src/main/java/com/fenrir/scissors/model/draw/shapetools/ArrowTool.java
+++ src/main/java/com/fenrir/scissors/model/draw/shapetools/ArrowTool.java
@@ -16,38 +16,41 @@ public class ArrowTool extends ShapeTool {
     protected void mouseDraggedEvent(MouseEvent event) {
         GraphicsContext graphicsContext = super.layer.getGraphicsContext2D();
         graphicsContext.clearRect(0, 0, super.layer.getWidth(), super.layer.getHeight());
-        graphicsContext.strokeLine(
-                startPoint.getKey(),
-                startPoint.getValue(),
-                event.getX(),
-                event.getY()
-        );
-        double dx = startPoint.getKey() - event.getX();
-        double dy = startPoint.getValue() - event.getY();
+
+        drawArrow(startPoint.getKey(), startPoint.getValue(), event.getX(), event.getY());
+    }
+
+    private void drawArrow(double x1, double y1, double x2, double y2) {
+        // vector
+        double dx = x1 - x2;
+        double dy = y1 - y2;
+
+        // distance between (x1, y1) and (x2, y2)
         double dist = Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2));
-        dx /= dist;
-        dy /= dist;
-        double x1 = event.getX() + 10 * dy;
-        double y1 = event.getY() - 10 * dx;
-        double x2 = event.getX() - 10 * dy;
-        double y2 = event.getY() + 10 * dx;
-        System.out.println("dist: " + dist);
-        System.out.println("dx: " + dx + "dy: " + dy);
-        System.out.println("Start: " + startPoint.getKey() + " " + startPoint.getValue());
-        System.out.println("end: " + event.getX() + " " + event.getY());
-        System.out.println("p1: " + x1 + " " + y1);
-        System.out.println("p2: " + x2 + " " + y2);
+        dx /= dist; //cos
+        dy /= dist; //sin
+
+        // endpoint of line
+        double x3 = x2 + 12 * dx;
+        double y3 = y2 + 12 * dy;
+
+        // two perpendicular points to the line on the end of this line,
+        // forming the base of the triangle
+        double x4 = x3 + 6 * dy;
+        double y4 = y3 - 6 * dx;
+        double x5 = x3 - 6 * dy;
+        double y5 = y3 + 6 * dx;
+
+        GraphicsContext graphicsContext = super.layer.getGraphicsContext2D();
+
+        // drawing triangle
         graphicsContext.fillPolygon(
-                new double[] {
-                        x1,
-                        x2
-                },
-                new double[] {
-                        y1,
-                        y2
-                },
-                2
+                new double[] {x4, x5, x2},
+                new double[] {y4, y5, y2},
+                3
         );
+
+        graphicsContext.strokeLine(x1, y1, x3, y3);
     }
 
     @Override
