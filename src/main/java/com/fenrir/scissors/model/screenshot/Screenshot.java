18bc6f81d2f8158c6b4cf536d6cb773da71584ea
Fenrir7734 <hitex1999@gmail.com>
2021-09-17 15:00:39 +0200

    new constructor

--- src/main/java/com/fenrir/scissors/model/screenshot/Screenshot.java
+++ src/main/java/com/fenrir/scissors/model/screenshot/Screenshot.java
@@ -10,20 +10,59 @@ import javafx.scene.layout.*;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+/**
+ * Class used to hold taken screenshot as {@link Image Image}.
+ *
+ * @author Fenrir7734
+ * @version v1.0.0 September 17, 2021
+ */
 public class Screenshot {
     private String name;
     private final Image image;
 
+    /**
+     * Create instance of this class.
+     *
+     * @param image Image.
+     * @param name  Name of the image.
+     */
     public Screenshot(Image image, String name) {
         this.image = image;
         this.name = name;
     }
 
+    /**
+     * Create instance of this class. Name of the image will be date in format: yyyy-MM-dd_HH:mm:ss.
+     *
+     * @param image Image.
+     */
     public Screenshot(Image image) {
         this.image = image;
         this.name = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss").format(new Date());
     }
 
+    /**
+     * Create instance of this class. Takes snapshot of the given canvas and holds it as an {@link Image Image}.
+     *
+     * @param canvas    Canvas to snapshot.
+     * @param name      Name of the image.
+     */
+    public Screenshot(Canvas canvas, String name) {
+        WritableImage image = new WritableImage((int) canvas.getWidth(), (int) canvas.getHeight());
+        canvas.snapshot(null, image);
+
+        this.image = image;
+        this.name = name;
+    }
+
+    /**
+     * Crops image to the given rectangular area. The width and height of the area cannot be greater than image size and
+     * must be greater them 0.
+     *
+     * @param area  The area based on which the image will be cropped.
+     *
+     * @return  New instance of this class holding cropped image.
+     */
     public Screenshot cropScreenshot(Area area) {
         if((area.getWidth() < image.getWidth() || area.getHeight() < image.getHeight())
                 && area.getWidth() > 0 && area.getHeight() > 0) {
@@ -42,12 +81,11 @@ public class Screenshot {
         return this;
     }
 
-    public Screenshot canvasSnapshot(Canvas canvas) {
-        WritableImage image = new WritableImage((int) canvas.getWidth(), (int) canvas.getHeight());
-        canvas.snapshot(null, image);
-        return new Screenshot(image, this.name);
-    }
-
+    /**
+     * Creates instance of {@link Background Background} class from image.
+     *
+     * @return  background
+     */
     public Background getScreenshotAsBackground() {
         return new Background(
                 new BackgroundImage(
@@ -60,14 +98,29 @@ public class Screenshot {
         );
     }
 
+    /**
+     * Gets image.
+     *
+     * @return image.
+     */
     public Image getImage() {
         return image;
     }
 
+    /**
+     * Gets name of the image.
+     *
+     * @return  name of the image.
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Sets name of the image.
+     *
+     * @param name  name to be set.
+     */
     public void setName(String name) {
         this.name = name;
     }
