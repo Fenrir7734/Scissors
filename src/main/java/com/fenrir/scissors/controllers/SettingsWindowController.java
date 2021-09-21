d1bf804cd8360e4aa0784de83c89f4338582d866
Fenrir7734 <hitex1999@gmail.com>
2021-09-21 19:50:20 +0200

    handle hyperlink and new fav layout

--- src/main/java/com/fenrir/scissors/controllers/SettingsWindowController.java
+++ src/main/java/com/fenrir/scissors/controllers/SettingsWindowController.java
@@ -1,16 +1,20 @@
 package com.fenrir.scissors.controllers;
 
+import com.fenrir.scissors.Scissors;
 import com.fenrir.scissors.model.Properties;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
 
@@ -26,6 +30,7 @@ import java.util.stream.Collectors;
  */
 public class SettingsWindowController {
     private static SettingsWindowController instance;
+    public TabPane settingsPane;
 
     @FXML private CheckBox automaticSaveCheckbox;
     @FXML private CheckBox automaticCopyCheckbox;
@@ -114,6 +119,17 @@ public class SettingsWindowController {
     }
 
     /**
+     * Handles action to open hyperlink.
+     *
+     * @param event event
+     */
+    @FXML
+    private void openHyperlink(ActionEvent event) {
+        String url = ((Hyperlink) event.getTarget()).getText();
+        Scissors.getInstance().getHostServices().showDocument(url);
+    }
+
+    /**
      * Control that displayed Favorite items placed in ListView are the same as these stored in properties file.
      *
      * @see com.fenrir.scissors.model.Properties.Favorite
@@ -148,22 +164,43 @@ public class SettingsWindowController {
      *                                                                                be added to ListView.
      */
     private void addFavorite(Properties.Favorite favorite) {
+        HBox item = createItem(favorite);
+        favoritesViewItems.add(item);
+        favoriteListView.setItems(favoritesViewItems);
+    }
+
+    /**
+     * Creates item containing name and path from {@link com.fenrir.scissors.model.Properties.Favorite}.
+     *
+     * @param favorite {@link com.fenrir.scissors.model.Properties.Favorite Favorite} class instance which content will
+     *                                                                                 be added to ListView.
+     * @return          HBox containing name and path from {@link com.fenrir.scissors.model.Properties.Favorite}.
+     */
+    private HBox createItem(Properties.Favorite favorite) {
         HBox item = new HBox();
         item.setId(favorite.name());
+        item.getStyleClass().add("favorite-container");
+
         Button button = new Button();
+        button.setAlignment(Pos.BOTTOM_CENTER);
+        button.getStyleClass().add("favorite-delete");
         button.setOnMouseClicked(e -> {
             removeFavorite(item.getId());
             MainWindowController.getInstance().refreshFavorites();
         });
 
-        item.getChildren().addAll(
-                new Label(favorite.name()),
-                new Label(favorite.path().toString()),
-                button
-        );
+        Label nameLabel = new Label(favorite.name());
+        nameLabel.getStyleClass().add("favorite-name");
 
-        favoritesViewItems.add(item);
-        favoriteListView.setItems(favoritesViewItems);
+        Label pathLabel = new Label(favorite.path().toString());
+        pathLabel.getStyleClass().add("favorite-path");
+
+        VBox nameContainer = new VBox(nameLabel, pathLabel);
+        nameContainer.getStyleClass().add("favorite-name-container");
+
+        item.getChildren().addAll(nameContainer, button);
+
+        return item;
     }
 
     /**
